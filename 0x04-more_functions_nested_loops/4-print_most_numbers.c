9602f5e3b137be2ed86ed93865acc020feb18bb8
Wanguileah <leahaminga@gmail.com>
2023-08-17 12:47:57 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,18 @@
 void print_most_numbers(void)
 
 {
+	
 	int x = 0;
 
 	for (; x <= 9; x++)
 	{
-	if (x == 2 || x ++ 4)
+	if (x == 2 || x == 4)
 	{
 	continue;
 	}
-	else{_putchar(x + '0');
+	else
+	{
+	_putchar(x + '0');
 	}
 	}
 	_putchar('\n');
